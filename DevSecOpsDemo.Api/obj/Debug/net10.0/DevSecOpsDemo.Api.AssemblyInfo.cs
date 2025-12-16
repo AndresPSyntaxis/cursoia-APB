37d741dc073d4e3e53be9accc869fcd1a5df7cf0
AndresPSyntaxis <andres@syntaxis.cl>
2025-12-16 20:49:59 -0300

    Archivos de build y evidencias agregadas

--- DevSecOpsDemo.Api/obj/Debug/net10.0/DevSecOpsDemo.Api.AssemblyInfo.cs
+++ DevSecOpsDemo.Api/obj/Debug/net10.0/DevSecOpsDemo.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevSecOpsDemo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9465bae02846cdfe7aa302fda349e7a313c3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevSecOpsDemo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevSecOpsDemo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
